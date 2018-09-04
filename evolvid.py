89ffdaf5b05c02f4e5906425440141b22371d035
Fraopl <navid.moeller@gmail.com>
2018-09-04 23:02:01 +0200

    added square to scene

--- evolvid.py
+++ evolvid.py
@@ -8,14 +8,17 @@ def main():
     size = width, height = 320, 240
 
     screen = pygame.display.set_mode(size)
-
+    
+    square = pygame.image.load("gfx/blue.png")
+    squarerect = square.get_rect()
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
         screen.fill((0, 0, 0))
-        #screen.blit(ball, ballrect)
+        screen.blit(square, squarerect)
         pygame.display.flip()
     
 
